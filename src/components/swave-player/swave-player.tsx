18046b52469b163e3f3df4bff5acd4704cf7321c
Cosmin Ioan Seviciu <cosmin.seviciu@3pillarglobal.com>
2019-04-04 16:42:52 +0300

    manage to make to integrate the swave api

--- src/components/swave-player/swave-player.tsx
+++ src/components/swave-player/swave-player.tsx
@@ -1,27 +1,36 @@
-import { Component, Prop } from '@stencil/core';
-import Swave from 'swave';
+import {Component, Prop} from "@stencil/core";
+import Swave from "swave";
 
 @Component({
-  tag: 'swave-player',
-  styleUrl: 'swave-player.scss',
+  tag: "swave-player",
+  styleUrl: "swave-player.scss",
   shadow: true
 })
 export class SwavePlayer {
   @Prop() audioUrl: string;
   private swave: any;
-  private hostElement: any;
 
-  componentWillLoad () {
-      this.hostElement = document.createElement('div').innerHTML = "omg"; // this is not good
-      this.swave = new Swave(this.hostElement); // this does not work
-      console.log(this.audioUrl)
+  componentWillLoad() {
+    this.swave = new Swave({audioUrl: this.audioUrl});
+    console.log(this.audioUrl)
   }
 
+  play() {
+    this.swave.play();
+  }
+
+  stop() {
+    this.swave.stop();
+  }
+
+  pause() {
+    this.swave.pause();
+  }
+
+
   render() {
-      console.log(this.audioUrl)
     return <div class='swave-player'>
-        does it still work?
-        {this.hostElement}
-        </div>;
+      <button onClick={() => this.play()}>Play</button>
+    </div>;
   }
 }
